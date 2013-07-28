fae9473bc4389168fa42c46e3be05629f6d05ef6
Sergey Khaladzinski <gansbrest@yahoo.com>
2013-07-28 17:17:50 -0400

    Complete rewrite of the tarsnapit script

--- tarsnapit.php
+++ tarsnapit.php
@@ -1,114 +1,147 @@
 #!/usr/bin/php
-
 <?php
 
+$options = getopt('c:b:d');
 
-################ CONFIG ##########################
-
-define('TARSNAP_BIN', '/usr/local/bin/tarsnap');
-define('DB_BACKUP_DIR', '/usr/local/backup/');
-define('EXCLUSIONS_FILE', '/usr/local/backup/exclusions.txt');
-define('DELETE_OLD_ARCH_AFTER', '30days');
-define('KEEP_AT_LEAST', '15days');
-
-# Perform mysql backups
-#
-#   Example:
-#
-#   'db1' => array(
-#       'username' => 'user1',
-#       'pass' => 'pass1',
-#    ),
-#
-$dbsList = array();
-
-# Files and folders to archive
-#
-#   Example:
-#
-#  'system' => array(
-#    '/etc',
-#    '/var/spool',
-#    '/usr/share/munin',
-#  ),
-#  'homedirs' => array(
-#    '/home/username',
-#    '/home/git',
-#  ),
-$filesAndFoldersArr = array();
-
-# Files and folders to exclude from backup 
-#
-#   Example:
-#
-#   '/var/www/site1/files/site1/imagecache',
-#
-#
-$excludesArr = array();
-
-
-
-#################### END CONFIG ########################
-
-if (count($dbsList)) {
-  foreach($dbsList as $dbName => $dbDataArr) {
-    system('mysqldump -u ' . $dbDataArr['username'] . ' -p'. $dbDataArr['pass'] . ' ' . $dbName . ' > ' . DB_BACKUP_DIR . $dbName . '.sql', $dbStatus);
-  }
+if (empty($options['c']) || (empty($options['b']) && !isset($options['d']))) {
+  check_status(1, "ERROR! \nTarsnapit works in 2 modes: \nbackup mode: tarsnapit.php -c /path/to/conf -b bundle_name \ncleanup mode: tarsnapit.php -c /path/to/conf -d\n");
 }
 
-$status = file_put_contents(EXCLUSIONS_FILE, implode("\n", $excludesArr));
-$excludeStatus = $status ? 0 : 1;
+$json_str = file_get_contents($options['c']);
+$config = json_decode($json_str, true);
 
-# Call tarsnap to create archive
-$archiveName = date('Y-m-d') . '-prod';
-$tarsnapError = 0; // Tarsnap should reset this flag later
+$tarsnap_bin = $config['global']['tarsnap_bin'];
 
-// Check for errors in previous steps
-if (empty($dbStatus) && empty($excludeStatus) && !empty($filesAndFoldersArr)) {
-  foreach($filesAndFoldersArr as $archiveSuffix => $fNamesList) { 
-    $cmd = TARSNAP_BIN . ' -c -f ' . $archiveName . '-' . $archiveSuffix . ' -X ' . EXCLUSIONS_FILE . ' ' . implode(' ', $fNamesList);
-    print $cmd . "\n";
-    system($cmd, $tarsnapStatus);
+// Mode selection
+if (isset($options['d'])) {
+  # CLEANUP MODE
+  //$packed_str = file_get_contents('/tmp/archives_list');
+  if (!empty($packed_str)) {
+    $archives_list = unserialize($packed_str);
+  } else {
+    exec($config['global']['tarsnap_bin'] . ' --list-archives | sort -t- -k 1,1nr -k 2,2nr -k 3,3nr -k 4,4nr -k 5,5nr -k 6,6nr', $archives_list);
 
-    // Errors during execution
-    if (!empty($tarsnapStatus)) {
-      $tarsnapError = 1;
+    // DEBUG
+    //Temporarily save output to file
+    //if (!empty($archives_list)) {
+      //file_put_contents('/tmp/archives_list', serialize($archives_list));
+    //}
+  }
+
+  if (!empty($archives_list)) {
+
+    // Allow plugins to extend config
+    foreach($config['bundles'] as $bundle_name => $bundle_data_arr) { 
+      $plugins_list = get_plugins_list($bundle_data_arr['plugins']);
+      foreach($plugins_list as $plugin) {
+        //  Load extra config
+        $plugin_config_arr = plugin_call($plugin, 'extendConfig');
+        if (!empty($plugin_config_arr)) {
+          $config['bundles'][$bundle_name]['groups'] += $plugin_config_arr;
+        }
+      }
+
+      // Doing cleanup
+      foreach($config['bundles'][$bundle_name]['groups'] as $group_name => $folders_arr) {
+        deleteOldArchives($archives_list, $bundle_name . '-' . $group_name, $bundle_data_arr['delete_after'], $bundle_data_arr['keep_at_least']);
+      }
     }
   }
 } else {
-  print 'Db status - ' . $dbStatus . "\n";
-  print 'Exclude status - '. $excludeStatus . "\n";
+  # BACKUP MODE
+  $config = $config['bundles'][$options['b']];
+  if (!empty($config)) {
+
+    // Create exclusions to be passed to tarsnap
+    if (!empty($config['excludes'])) {
+      $exclusions_file = '/tmp/tarsnap-' . $options['b'] . '-excludes';
+      $bytes_wrtn = file_put_contents($exclusions_file, implode("\n", $config['excludes']));
+      if ($bytes_wrtn > 0) {
+        $status = 0;
+      }
+      check_status($status, 'Problem with exclusions file');
+    }
+
+    // Init plugins
+    $plugins_list = get_plugins_list($config['plugins']);
+
+    foreach($plugins_list as $plugin) {
+      //  Load extra config
+      $plugin_config_arr = plugin_call($plugin, 'extendConfig');
+      if (!empty($plugin_config_arr)) {
+        $config['groups'] += $plugin_config_arr;
+      }
+
+      // Execute plugin
+      $status = plugin_call($plugin, 'execute');
+      check_status($status, "Problem during $plugin->name execution.");
+    }
+
+    # Call tarsnap to create archive
+    $archiveName = date('Y-m-d-H-i-s') . '-prod-' . $options['b'];
+
+    foreach($config['groups'] as $archiveSuffix => $fNamesList) { 
+      $cmd = $tarsnap_bin . ' -c -f ' . $archiveName . '-' . $archiveSuffix . ' -X ' . $exclusions_file . ' ' . implode(' ', $fNamesList);
+      print $cmd . "\n";
+      system($cmd, $status);
+
+      check_status($status, 'Problem with ' . $archiveSuffix . ' group.');
+    }
+
+    # Tarsnapit cleanup
+    unlink($exclusions_file);
+
+    # Plugins cleanup
+    foreach($plugins_list as $plugin) {
+      plugin_call($plugin, 'cleanup');
+    }
+  }
 }
 
-# Cleanup
-if (empty($tarsnapError)) {
-  system('rm -rf ' . DB_BACKUP_DIR . '*');
-  deleteOldArchives(DELETE_OLD_ARCH_AFTER);
-}
+function deleteOldArchives($archivesList, $filterName, $delete_after, $keep_at_least) {
+  global $config;
 
-function deleteOldArchives($dateStr, $secureDateStr = KEEP_AT_LEAST) {
+  $backupItemsList = array();
+  if (!empty($archivesList)) {
+    foreach($archivesList as $backupItem) {
+      if (preg_match("|$filterName|is", $backupItem)) {
+        $backupItemsList[] = $backupItem;
+      }
+    }
+  }
 
-  exec(TARSNAP_BIN . ' --list-archives | sort -t- -k 1,2nr -k 2,3nr -k 3,4nr', $archivesList);
+  if (empty($backupItemsList)) {
+    echo 'No backup items - terminate';
+    exit;
+  }
 
-  $latestBackupDate = new DateTime(extractArchiveDateStr($archivesList[0]));
+  $latestBackupDate = new DateTime(extractArchiveDateStr($backupItemsList[0]));
   $maxAllowedDate = clone $latestBackupDate;
-  $maxAllowedDate->modify('-' . $dateStr);
+  $maxAllowedDate->modify('-' . $delete_after);
 
-  // Security check to prevent backup removals in case 
-  // some backup system failures
-  $lastArchiveItem = array_pop($archivesList);
+  // Security check to prevent backup removals in case of
+  // backup system failures
+  $lastArchiveItem = array_pop($backupItemsList);
   $oldestArchiveDate = new DateTime(extractArchiveDateStr($lastArchiveItem));
-  array_push($archivesList, $lastArchiveItem);
+  array_push($backupItemsList, $lastArchiveItem);
 
   $currDate = new DateTime();
   $secureDate = clone $currDate;
-  $secureDate->modify('-' . $secureDateStr);
+  $secureDate->modify('-' . $keep_at_least);
 
-  if ($oldestArchiveDate < $secureDateStr) {
+  if ($oldestArchiveDate < $secureDate) {
     return; // Keep at least $secureDateStr of backups :)
   }
 
-  foreach($archivesList as $archiveName) {
+  // HELPFUL DEBUG OPTIONS
+  //echo "Oldest " . $oldestArchiveDate->format('Y-m-d-H-i-s') . "\n";
+  //echo "Secure date " . $secureDate->format('Y-m-d-H-i-s') . "\n";
+  //echo __FILE__."<pre>"; print_r($oldestArchiveDate->format('Y-m-d-H-i-s')); "</pre>";
+  //exit;
+
+  ## Archives cleanup
+  $archToRemoveList = array();
+  foreach($backupItemsList as $archiveName) {
     $archiveDate = new DateTime(extractArchiveDateStr($archiveName));
     if ($archiveDate < $maxAllowedDate) {
       $archToRemoveList[] = $archiveName;
@@ -121,7 +154,52 @@ function deleteOldArchives($dateStr, $secureDateStr = KEEP_AT_LEAST) {
 }
 
 function extractArchiveDateStr($archiveName) {
-  return substr($archiveName, 0, 10);
+  $time_str = substr($archiveName, 0, 19);
+  $time_parts = explode('-', $time_str);
+  return "$time_parts[0]-$time_parts[1]-$time_parts[2] $time_parts[3]:$time_parts[4]:$time_parts[5]";
+}
+
+function plugin_call($plugin, $method_name) {
+  $result = '';
+  if (is_callable(array($plugin, $method_name))) {
+    $result = $plugin->$method_name();
+  }
+  return $result;
+}
+
+function get_plugins_list($plugin_files_list = array()) {
+  $plugins_list = array();
+
+  if (!empty($plugin_files_list)) {
+    foreach($plugin_files_list as $plugin) {
+      // Load plugin
+      require_once($plugin);
+      // Get plugin class name for initialization
+      $plugin_class = get_plugin_class_name($plugin);
+
+      $plugin = new $plugin_class();
+      $plugins_list[] = $plugin;
+    }
+  }
+
+  return $plugins_list;
+}
+
+function get_plugin_class_name($plugin) {
+  $basename =  basename($plugin, '.php');
+  $basename =  str_replace('_', ' ', $basename);
+  $class_name =  ucwords($basename);
+  $class_name =  str_replace(' ', '_', $class_name);
+  return $class_name;
+}
+
+function check_status($status, $err_msg = '') {
+  if ($status > 0) {
+    if (!empty($err_msg)) {
+      echo "$err_msg\n";
+    }
+    exit($status);
+  }
 }
 
 ?>
